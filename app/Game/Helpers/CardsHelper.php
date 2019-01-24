74d51de2f882cc209c5bf7cd762ee1d629b72dbf
Gullon <me@alecgullon.co.uk>
2019-01-24 20:00:49 +0000

    Documentation for the Game helper classes

--- app/Game/Helpers/CardsHelper.php
+++ app/Game/Helpers/CardsHelper.php
@@ -2,8 +2,18 @@
 
 namespace App\Game\Helpers;
 
+/**
+ * Helper class that provides some methods related to manipulating sets of Card models
+ */
 class CardsHelper {
 
+    /**
+     * Takes an array of Card models and sorts these according to their names
+     *
+     * @param   array       $cardStack
+     *
+     * @return  array
+     */
     public static function sortCardStackByName($cardStack) {
         usort($cardStack, function($a, $b) {
             if ($a->name() < $b->name()) {
--- app/Game/Helpers/StringHelper.php
+++ app/Game/Helpers/StringHelper.php
@@ -2,8 +2,20 @@
 
 namespace App\Game\Helpers;
 
+/**
+ * Helper class that provides some methods related to the manipulation and creation
+ * of certain types of strings
+ */
 class StringHelper {
 
+    /**
+     * Builds a cards class alias from its stub - e.g., 'council-room' becomes 'CouncilRoom'. This alias
+     * is then used in class names related to this card - e.g., CouncilRoomController
+     *
+     * @param   string      $stub
+     *
+     * @return  string
+     */
     public static function cardAliasFromStub($stub) {
         $parts = explode('-', $stub);
 
@@ -15,6 +27,14 @@ class StringHelper {
         return $alias;
     }
 
+    /**
+     * Takes a string which represents some kind of in game $action corresponding to an action card
+     * and builds the corresponding method from it - e.g., 'workshop/gain-card' becomes 'gainCard'
+     *
+     * @param   string      $action
+     *
+     * @return  string
+     */
     public static function methodFromCardAction($action) {
         $action = explode('/', $action)[1];
         $parts = explode('-', $action);
@@ -27,6 +47,15 @@ class StringHelper {
         return lcfirst($method);
     }
 
+    /**
+     * Describes the given array of cards in a log friendly format - e.g., ['estate', 'estate', 'village']
+     * becomes 'two Estates and a Village'. The alphabetical order of the elements is determined by
+     * the first appearance of each element in the $cardStack array
+     *
+     * @param   array       $cardStack
+     *
+     * @return  string
+     */
     public static function describeCardStack($cardStack) {
         $cardAmounts = [];
         foreach ($cardStack as $card) {
